9fcf95db8e92a6e26cf3c8eee8eae2d5ead54279
currysesame <60995715+currysesame@users.noreply.github.com>
2022-12-10 20:28:10 +0900

    Add files via upload
    Add read text file content.

--- create_video.py
+++ create_video.py
@@ -7,26 +7,32 @@ import glob
 
 """以下2行はwindowsユーザは書かないとおそらくエラーが出ます"""
 from moviepy.config import change_settings
-change_settings({"IMAGEMAGICK_BINARY": r"C:/D_server/Program_Files/imagemagick/ImageMagick-7.1.0-Q16-HDRI/magick.exe"})
+change_settings({"IMAGEMAGICK_BINARY": r"D:/PRO/ImageMagick/ImageMagick-7.1.0-Q16-HDRI/magick.exe"})
 
 file_path = glob.glob("./*.mp4")[0]#字幕を付けたい動画のパス
+text_path = glob.glob('./*.txt')[0]#字幕本体
 
 """字幕の設定関数"""
 def annotate(clip, txt, txt_color='black', fontsize=50, font='Xolonium-Bold'):
     #Writes a text at the bottom of the clip. 
     txtclip = editor.TextClip(txt, fontsize=fontsize, font=font, color=txt_color)
     cvc = editor.CompositeVideoClip([clip, txtclip.set_pos(('center', 'bottom'))])
-
     return cvc.set_duration(clip.duration)
 
 video = VideoFileClip(file_path)
-def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-#subs・・・字幕を入れる時刻と文字列のタプル。(開始時刻,終了時刻),'表示したい文字'
-subs = [((0, 4), 's'),
-        ((4, 8), 'subs2'),
-        ((8, 10), 'subs3'),
-        ((10, 12), 'subs4')]#動画の長さまで字幕を設定する。途中で切られる。表示したくないときは空文字''で
 
-annotated_clips = [annotate(video.subclip(from_t, to_t), txt) for (from_t, to_t), txt in subs]#動画と字幕を繋げる処理
+
+text_file = open(text_path, "r")
+lasttime = 0
+subtitles = []
+for line in text_file:
+	linesegment = line.split()
+	if len(linesegment) == 1:
+		subtitles.append(((lasttime, float(linesegment[0])), ' '))
+	else:
+		subtitles.append(((lasttime, float(linesegment[0])), linesegment[1]))
+	lasttime = float(linesegment[0])
+
+annotated_clips = [annotate(video.subclip(from_t, to_t), txt) for (from_t, to_t), txt in subtitles]#動画と字幕を繋げる処理
 final_clip = editor.concatenate_videoclips(annotated_clips)
-final_clip.write_videofile(file_path[:-4] + '_sub.mp4')
+final_clip.write_videofile("movie_withSubtitle.mp4")
